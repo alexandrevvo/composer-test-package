f0dd034b25eba5b37c2cd32182fbff6136e12454
alexandrevvo <alexandrevvo@gmail.com>
2021-02-24 20:09:06 +0000

    first

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '99.9.10',
+    'version' => '99.9.10.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'avvo/test-package',
+  ),
+  'versions' => 
+  array (
+    'avvo/test-package' => 
+    array (
+      'pretty_version' => '99.9.10',
+      'version' => '99.9.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
